dd21608a87f8ea2fabed61a3e0f740083f30ea9c
Makendoso <makendoshirakawa@gmail.com>
2026-05-24 09:23:52 -0700

    Fix allowed remote image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: false,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+      },
+    ],
   },
 }
 
